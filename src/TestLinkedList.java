93fa2ff9544eacd6a92f066279c0c1411a1c9a03
mingy <249127381@qq.com>
2020-08-08 20:07:14 +0800

    test2

--- src/TestLinkedList.java
+++ src/TestLinkedList.java
@@ -4,7 +4,7 @@ public class TestLinkedList {
     public static void main(String[] args) {
         LinkedList<Integer> integers = new LinkedList<>();
         integers.add(89);
-        integers.add(67);
+        integers.add(66);
 
     }
 }
